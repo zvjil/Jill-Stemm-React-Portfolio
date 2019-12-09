a1ba15510516302a5dc69ae450362c2c94047b97
Jill <zvjil@hotmail.com>
2019-12-09 04:09:02 -0500

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Jill Stemm Portfolio</h1>
+                  React 
+       
+      </div>
+    );
+  }
+}
